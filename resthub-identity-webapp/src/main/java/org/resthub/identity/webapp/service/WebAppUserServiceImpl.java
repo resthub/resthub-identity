53fb4ac636d1ad8349553f99a87525e79fb5c1f8
Sébastien Deleuze <seb@deleuze.fr>
2013-03-08 11:59:42 +0100

    Filename refactoring

---
+++ resthub-identity-webapp/src/main/java/org/resthub/identity/webapp/service/WebAppUserServiceImpl.java
@@ -0,0 +1,48 @@
+package org.resthub.identity.webapp.service;
+
+import org.resthub.identity.core.event.UserEvent;
+import org.resthub.identity.model.User;
+import org.resthub.identity.core.repository.UserRepository;
+import org.resthub.identity.core.service.AbstractGenericUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.elasticsearch.client.Client;
+import org.resthub.identity.webapp.elasticsearch.Indexer;
+import org.springframework.context.ApplicationListener;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+public class WebAppUserServiceImpl extends AbstractGenericUserServiceImpl<User, UserRepository> implements ApplicationListener<UserEvent> {
+
+    private @Value("#{esProp['index.name']}") String indexName;
+    private @Value("#{esProp['index.user.type']}") String indexType;
+    @Autowired
+    Client client;
+
+    /**
+     * Injection of elasticsearch indexer;
+     */
+    @Inject
+    @Named("elasticIndexer")
+    private Indexer indexer;
+
+    @Override
+	@Inject
+	@Named("userRepository")
+	public void setRepository(UserRepository userRepository) {
+		super.setRepository(userRepository);
+	}
+
+    @Override
+    public void onApplicationEvent(UserEvent event) {
+        User user = event.getUser();
+        if(event.getType() == UserEvent.UserEventType.USER_CREATION) {
+            indexer.add(client, user, indexName, indexType, user.getId().toString());
+        } else if(event.getType() == UserEvent.UserEventType.USER_UPDATE) {
+            indexer.edit(client, user, indexName, indexType, user.getId().toString());
+        } else if(event.getType() == UserEvent.UserEventType.USER_DELETION) {
+            indexer.delete(client, indexName, indexType, user.getId().toString());
+        }
+    }
+}
