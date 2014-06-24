2a4a357c67f5f0241edfa45d3d3706b74ba31814
bcecchinato <bastien@kekina.to>
2014-06-24 21:36:38 +0200

    Adding missing @Profile on repository

--- resthub-identity-core/src/main/java/org/resthub/identity/core/repository/defaults/DefaultApplicationRepository.java
+++ resthub-identity-core/src/main/java/org/resthub/identity/core/repository/defaults/DefaultApplicationRepository.java
@@ -2,9 +2,11 @@ package org.resthub.identity.core.repository.defaults;
 
 import org.resthub.identity.core.repository.ApplicationRepository;
 import org.resthub.identity.model.Application;
+import org.springframework.context.annotation.Profile;
 
 import javax.inject.Named;
 
+@Profile(value = "resthub-identity-user")
 @Named("applicationRepository")
 public interface DefaultApplicationRepository extends ApplicationRepository<Application, Long> {
 
